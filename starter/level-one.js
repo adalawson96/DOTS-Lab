6c600088b821059327bc58a7f702c58858043b81
adalawson <adalawson96@gmail.com>
2023-10-28 12:29:12 +0200

    get the score to console log on three levels

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,25 @@
  * DOTS: Level One
  *
  */
+
+let score = 0;
+const ball = document.querySelector('.js-ball');
+const scoreDisplay = document.getElementsByClassName('.js-score');
+const levelWinner = document.getElementsByClassName('.level-winner');
+
+
+ball.addEventListener('click', function() {
+    // some logic for updating score and checking for a win here
+    // increment the score by 10
+    score += 10;
+    // Update the score display to show the updated score
+    scoreDisplay.innerText = score;
+     // Log the score to the console for verification
+    console.log("Score incremented by 10. New score: " + score);
+    
+    if(score >= 100){
+        levelWinner.style.opacity = 1;
+    } else{
+        console.log('Score:', score);
+    }
+  });
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,27 @@
  * DOTS: Level Two
  *
  */
+
+const elements = document.querySelectorAll(".js-ball");
+// const ball = document.querySelector('level-arena js-arena');
+const scoreDisplay = document.getElementsByClassName('.level-header__score');
+const levelWinner = document.getElementsByClassName('.level-winner');
+let score = 0;
+
+
+console.log(elements)
+
+for(let i = 0; i < elements.length ; i++){
+    elements[i].addEventListener('click', function(){
+        score += 10;
+        scoreDisplay.innerText = score;
+        console.log("Score incremented by 10. New score: " + score);
+        
+        if(score >= 100){
+          levelWinner.style.opacity = 1;
+        } else {
+            console.log('Score:', score);
+        }
+
+    })
+}
